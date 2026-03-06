5f6ff4ffcaabbbf9e45c5bdfca8901b5e0e6d56f
Jaci26cx <Ja824325@ucf.edu>
2026-03-05 23:29:57 -0500

    had to create new repo cuz other one wouldnt display changes

---
+++ exercise-app/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App () {
+
+//Didn't do lastweeks assignment but wanted to learn how to export something onto gh pages
+  return (
+    <div className="App">
+  
+        <p>
+          Select from the list of Exercises:
+        </p>
+        <ul>
+        <li><button>Push Ups</button></li>
+        <li><button>Running</button></li>
+        <li><button>Planks</button></li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
---
+++ exercise-app/src/components/DurationExercise.js
@@ -0,0 +1,12 @@
+/*Didn't get to this part, got frusrtated trying to figure out how to connect the pages via parameter
+
+import {useState, useEffect} from "react";
+
+function counter() {}
+const [count, setCount] = useState (0);
+
+useEffect(()) => {
+ 
+    }
+
+*/
